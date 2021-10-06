d0aed7650ca2fa724c76ed6ed917119a6950a8dc
Sandrine Boutier <sandrineboutier@gmail.com>
2021-10-06 17:22:55 +0200

    Sandrine pushin files

---
+++ addLinkForm.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width">
+  <title>addLinkForm</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<header>
+<?php include 'banner1.php';?>
+      <nav class="banner2">
+        <a href="index.php">ACCUEIL </a>
+        >
+        <a>Ajouter une ressource</a>
+      </nav>
+  
+</header>
+
+  <div id="addLinkForm">
+    <form>
+      
+      <div>
+        <label for="category-select">Catégorie</label><br />
+        <select class="addLinkFormField" name="categories">
+          <option value=" ">Sélectionnez une catégorie</option>
+          <option value="languages">Langages</option>
+          <option value="others">Autres thématiques</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="subCategory-select">Sous-catégorie</label><br />
+        <select class="addLinkFormField" name="subCategories">
+          <option value=" ">Sélectionnez une sous-catégorie</option>
+          <option value="design">Design</option>
+          <option value="sharingCode">Partager du code</option>
+          <option value="picsIcons">Images et Icônes</option>
+          <option value="sharingCode">Partager du code</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="link-title">Titre du lien</label><br />
+        <input class="addLinkFormField" type="text" placeholder="Titre du lien" name="link-title" value="" />
+      </div>
+
+      <div>
+        <label for="link-address">Adresse du lien</label><br />
+        <input class="addLinkFormField" type="text" placeholder="Adresse du lien" name="link-address" value="" />
+      </div>
+
+      <div>
+        <label for="link-description">Description</label><br />
+        <textarea placeholder="Description du contenu proposé" id="link-description" name="link-description" value=""
+          rows="60"></textarea>
+      </div>
+
+      <div>
+        <input class="submitButton" type="submit" value="Poster" />
+      </div>
+
+    </form>
+
+  </div>
+
+</body>
+<script src="script.js"></script>
+
+</html>
---
+++ contactForm.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire de contact</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="pictures">
+<div>
+    <img src=photos/sandrine.png alt="Sandrine Designeuse">
+    <figcaption>Sandrine <br> Designeuse</figcaption>
+    
+    
+</div>
+
+    <div><img src=photos/falilou.png alt="Falilou Développeur">
+<figcaption>Falilou <br> Web Codeur</figcaption>
+
+        
+</div>
+
+<div>
+    <img src=photos/michel.png alt="Michel Chef de projet">
+<figcaption>Michel <br> Chef de Projet</figcaption>
+
+
+</div>
+</div>
+
+<h1 class="formulaireDeContactTitle">Formulaire de Contact</h1>
+
+<div class="form">
+    
+    <form  action="formulaire" method="post">
+
+        <div>
+            <p class="contact">Nom</p>
+             <input type="text" placeholder =" Nom" id="name" name="user_name">
+        </div>
+
+        <div>
+            <p class="contact">Numéro de téléphone</p>
+            <input type="email" placeholder="  Email" id="mail" name="user_mail">
+        </div>
+
+        <div>
+           <p class="contact">Numéro de téléphone</p>
+            <input type="PhoneNumber" placeholder="  Numéro de téléphone" id="phone" name="phone_number">
+        </div>
+
+        <div class="message">
+            
+            <p class="contact">Message</p>
+           <textarea type="message" placeholder=" Message"></textarea>
+        </div>
+
+        <div class="submit">
+            <button type="button">Envoyer le message</button>
+
+        </div>
+    </form>
+</div>
+<footer>
+
+</footer>
+</body>
+</html>
+@SandrineBoutier
+Attach files by dragging & dropping, selecting or pasting them.
